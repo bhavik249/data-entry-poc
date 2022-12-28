47d7cb97fe1a65cd568209f8654e134ba7170588
Bhavik Patel <patel.bhavik2492@gmail.com>
2022-12-28 17:05:34 +0530

    Remove unnecessary folder

--- data-entry-app/data_manager/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from data_entry.models import BaseModel
-
-class Events(BaseModel):
-    country = models.CharField(max_length=255)
-    title = models.CharField(max_length=255)
-    date = models.DateField()
-    notes = models.TextField(null=True, blank=True)
-    bunting = models.BooleanField(default=False)
